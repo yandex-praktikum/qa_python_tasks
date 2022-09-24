755eb825ce3402c2312537995d8a9998a459ff11
m-mukhanova <35366943+m-mukhanova@users.noreply.github.com>
2022-09-24 16:45:41 +0300

    Update practice_exercise_2.py

--- practice_exercise_2.py
+++ practice_exercise_2.py
@@ -1,7 +1,9 @@
-from selenium.webdriver.common.by import By
+import random
+
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.get("https://qa-mesto.praktikum-services.ru/")
@@ -12,13 +14,17 @@ driver.get("https://qa-mesto.praktikum-services.ru/")
 # Добавь явное ожидание для загрузки страницы
 ...
 
+# Запомни title последней карточки
+title_before = ...
+
 # Кликни по кнопке добавления нового контента
 driver.find_element(...)...
 
-# Введи название нового места
+# сгенерируй новое место и введи его в поле названия
+new_title = ...
 driver.find_element(...)...
 
-# Введи ссылку на изображение
+# В поле ссылки на изображение введи ссылку
 driver.find_element(...)...
 
 # Сохрани контент
@@ -27,7 +33,21 @@ driver.find_element(...)...
 # Дождись появления кнопки удаления карточки
 WebDriverWait(...).until(...)
 
-# Удали контент
+# Проверь, что на карточке отображается верное название
+title_after = ...
+assert ...
+
+# Запомни количество карточек до удаления
+cards_before = len(...)
+
+# Удали карточку
 driver.find_element(...)...
+
+# Дождись, что title последней карточки равен title_before
+WebDriverWait(...).until(...)
+
+# Проверь, что количество карточек стало на одну меньше
+cards_after = len(...)
+assert
 
 driver.quit()
